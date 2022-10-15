a70cd3f447b22c05d1f03f6754cd3c9c16ac9f77
Oleksii Biletskyi <alenikbel@ukr.net>
2022-10-15 17:45:07 +0300

    js hw11

--- src/index.js
+++ src/index.js
@@ -60,3 +60,78 @@ async function eventHandler(e) {
 }
 
 searchForm.addEventListener('submit', eventHandler);
+
+function renderGallery(name) {
+  const markup = name.hits
+    .map(hit => {
+      return `<div class="photo-card">
+
+        <a class="gallery-item" href="${hit.largeImageURL}">
+          <img
+            class="gallery__image"
+            src="${hit.webformatURL}"
+            alt="${hit.tags}"
+            loading="lazy"
+        /></a>
+
+        <div class="info">
+          <div class="info__box">
+            <p class="info-item">
+              <b class="material-symbols-outlined">thumb_up</b>
+            </p>
+            <p class="info-counter">${hit.likes.toLocaleString()}</p>
+          </div>
+
+          <div class="info__box">
+            <p class="info-item">
+              <b class="material-symbols-outlined">visibility</b>
+            </p>
+            <p class="info-counter">${hit.views.toLocaleString()}</p>
+          </div>
+
+          <div class="info__box">
+            <p class="info-item">
+              <b class="material-symbols-outlined">forum</b>
+            </p>
+            <p class="info-counter">${hit.comments.toLocaleString()}</p>
+          </div>
+
+          <div class="info__box">
+            <p class="info-item">
+              <b class="material-symbols-outlined">download</b>
+            </p>
+            <p class="info-counter">${hit.downloads.toLocaleString()}</p>
+          </div>
+
+        </div>
+      </div>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+// Load more button - function
+
+loadBtn.addEventListener(
+  'click',
+  () => {
+    name = searchQuery.value;
+    page += 1;
+    fetchImages(name, page, perPage).then(name => {
+      let totalPages = name.totalHits / perPage;
+      renderGallery(name);
+      new SimpleLightbox('.gallery a');
+      if (page >= totalPages) {
+        loadBtn.style.display = 'none';
+        Notiflix.Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    });
+  },
+  true
+);
+
+// Preloader
+
+window.addEventListener('load', fadeEffect);
